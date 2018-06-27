791e89f6fcb1e416feb2fa379127bd492ac6f7d2
Oleg Larkin <olegollar@gmail.com>
2018-06-27 09:38:53 +0300

    dependencies dev 3

--- addon/utils/image-resize.js
+++ addon/utils/image-resize.js
@@ -1,82 +1,53 @@
-// import Pica from 'pica';
+import Pica from 'pica';
 
 export default function imageResize(
   image,
   { maxWidth = 800, maxHeight = 600 }
 ) {
-  // const resizer = new Pica();
-  // const offScreenCanvas = document.createElement('canvas');
-  // offScreenCanvas.width = maxWidth;
-  // offScreenCanvas.height = maxHeight;
-  // return resizer
-  //   .resize(image, offScreenCanvas)
-  //   .then(result => resizer.toBlob(result, image.type, 0.9));
+  const resizer = new Pica();
 
-  const promise = new Promise((yea, nah) => {
-    var img = new Image();
-    var imageUrl = URL.createObjectURL(image);
+  return new Promise(res => {
+    const sourceCanvas = document.createElement('canvas');
+    const sctx = sourceCanvas.getContext('2d');
+    const img = new Image();
 
-    const IMG_MAX_WIDTH = maxWidth;
-    const IMG_MAX_HEIGHT = maxHeight;
-
-    if (image.type.indexOf('image') < 0) {
-      return nah();
-    }
+    const offScreenCanvas = document.createElement('canvas');
+    offScreenCanvas.width = maxWidth;
+    offScreenCanvas.height = maxHeight;
 
     img.onload = function() {
-      var oc = document.createElement('canvas'),
-        octx = oc.getContext('2d');
+      sourceCanvas.width = img.width;
+      sourceCanvas.height = img.height;
+      sctx.drawImage(img, 0, 0);
 
-      var image_width = img.width,
-        image_height = img.height;
-
-      if (img.width >= img.height) {
-        var imgRatioW = img.width / IMG_MAX_WIDTH;
-        image_width = IMG_MAX_WIDTH;
-        image_height = img.height / imgRatioW;
-      } else if (img.height > img.width) {
-        var imgRatioH = img.height / IMG_MAX_HEIGHT;
-        image_width = img.width / imgRatioH;
-        image_height = IMG_MAX_HEIGHT;
-      }
-
-      oc.width = image_width;
-      oc.height = image_height;
-
-      octx.drawImage(img, 0, 0, oc.width, oc.height);
-
-      return oc.toBlob(
-        blob => {
-          Object.defineProperties(blob, {
-            type: {
-              value: image.type,
-            },
-            name: {
-              value: image.name,
-            },
-            lastModified: {
-              value: image.lastModified,
-            },
-
-            width: {
-              value: image_width,
-            },
-
-            height: {
-              value: image_height,
-            },
-          });
-
-          return yea(blob);
-        },
-        {
-          type: image.type,
-        }
-      );
+      res({ sourceCanvas, offScreenCanvas });
     };
+    img.src = URL.createObjectURL(image);
+  })
+    .then(({ sourceCanvas, offScreenCanvas }) =>
+      resizer
+        .resize(sourceCanvas, offScreenCanvas)
+        .then(result => resizer.toBlob(result, image.type, 0.9))
+    )
+    .then(blob =>
+      Object.defineProperties(blob, {
+        type: {
+          value: image.type,
+        },
+        name: {
+          value: image.name,
+        },
+        lastModified: {
+          value: image.lastModified,
+        },
 
-    img.src = imageUrl;
-  });
+        // width: {
+        //   value: image_width,
+        // },
 
-  return promise;
+        // height: {
+        //   value: image_height,
+        // },
+      })
+    );
 }
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
   name: 'image-resize-util',
   included() {
     this._super.included.apply(this, arguments);
+    this.import('node_modules/pica/dist/pica.js');
     this.import('vendor/shims/pica.js');
   },
 };
